83e3ddbe3472c41eb5cdf09cbdd16281614d867a
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-27 21:24:29 +0200

    almost header complete

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const moreItemsArr = [
+  {
+    img: "../assets/assets/img/oculus_case.jpg",
+    description:
+      "Oculus Quest 2 Carrying Case for Lightweight, Portable Protection - VR Oculus Quest 2 Carrying Case for Lightweight, Portable Protection - VR",
+    stars: "four-five-stars",
+    votes: "7,808",
+    price: "49.00",
+  },
+  {
+    img: "../assets/assets/img/oculus_case.jpg",
+    description:
+      "Oculus Quest 2 Elite Strap for Enhanced Support and Comfort in VR Oculus Quest 2 Elite Strap for Enhanced Support and Comfort in VR",
+    stars: "four-five-stars",
+    votes: "8,918",
+    price: "49.00",
+  },
+  {
+    img: "../assets/assets/img/oculus_case.jpg",
+    description:
+      "Oculus Quest 2 Elite Strap for Enhanced Support and Comfort in VR Oculus Quest 2 Elite Strap for Enhanced Support and Comfort in VR",
+    stars: "four-five-stars",
+    votes: "8,918",
+    price: "49.00",
+  },
+];
+
+// const carousel = document.querySelector(".carousel");
+// moreItemsArr.map(
+//   (item) => ` <
+// `
+// );
